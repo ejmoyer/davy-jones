900bc4c667198873d476e55b4c2949898ec31a19
Eric Moyer <ejmoyer3@comcast.net>
2020-02-08 16:38:32 -0500

    Added github url to setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     version="0.0.1",
     author="Eric Moyer",
     author_email="ejmoyer3@comcast.net",
-    url="https://github.com/yourusername/yourproject",
+    url="https://github.com/ejmoyer/davy-jones",
     description="Gives Bash more of a GUI.",
     long_description=long_description,
     long_description_content_type="text/markdown",
